2b4afefad098d9b7db8e38a9e44c0a4429a9a396
mohamedghaly140 <mohamedghaly140@gmail.com>
2022-10-26 00:45:07 +0200

    create new app with upgraded expo sdk

--- App.js
+++ App.js
@@ -1,74 +1,20 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-};
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRound] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-        onError={err => console.log(err)}
-      />
-    );
-  }
-
-  const restartNewGameHandler = () => {
-    setGuessRound(0);
-    setUserNumber(null);
-  };
-
-  const startGameHandler = selectedNumber => {
-    setUserNumber(selectedNumber);
-  };
-
-  const gameOverHandler = numOfRounds => {
-    setGuessRound(numOfRounds);
-  };
-
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
-
-  if (userNumber && guessRounds <= 0) {
-    content = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
-  } else if (guessRounds > 0) {
-    content = (
-      <GameOverScreen
-        roundsNumber={guessRounds}
-        userNumber={userNumber}
-        onRestart={restartNewGameHandler}
-      />
-    );
-  }
-
   return (
-    <View style={styles.screen}>
-      <Header title='Guess a Number' />
-      {content}
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
