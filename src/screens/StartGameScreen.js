3feae6ee5cdda1d81887f1619b691bdf5c5676b7
mohamedghaly140 <mohamedghaly140@gmail.com>
2022-10-26 01:05:36 +0200

    added main screens

---
+++ src/screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
